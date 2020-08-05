dd9d55d6d4260dc9191bacb973dfcd5ae967fb92
Cameron King <cameronking42@gmail.com>
2020-08-05 17:04:19 -0500

    StoreList successfully opens an AceEditor, though the editor isn't picking up source code for the editing tools, nor is it handling non-component code well.

--- src/StoreList.svelte
+++ src/StoreList.svelte
@@ -1,15 +1,41 @@
 <script>
-import { createEventDispatcher } from 'svelte';
-const dispatch = createEventDispatcher();
+import { onMount } from 'svelte';
+export let gl_container, gl_layout;
 
 $: deps = Object.keys($__DIS__).sort();
 
+onMount(() => {
+    if (!deps['ioc-editing-tools/openDependency']) {
+        __DIS__.replace('ioc-editing-tools/openDependency', async (layout, path, event) => {
+            const newTab = event.ctrlKey;
+            const content = await __DIS__.lookupSource(path);
+
+            // initializing components should be hidden behind some kind of interface,
+            // since layout-intercept employs a bridge component that instantiates the concrete component
+            // through dependency injection via window.__DIS__
+            const aceItem = __DIS__.get()['layout-intercept/makeCmp']('ioc-editing-tools/AceEditor', { content, path, lm_title: path });
+
+            const baseRow = layout.root.contentItems[0];
+            if (baseRow.contentItems.length < 3) {
+                baseRow.addChild({
+                    type: 'column',
+                    content: [aceItem]
+                }, 1)
+            } else {
+                if (newTab)
+                    baseRow.contentItems[1].addChild(aceItem);
+                else
+                    baseRow.contentItems[1].replaceChild(baseRow.contentItems[1].contentItems[0], aceItem);
+            }
+        });
+    }
+});
 </script>
 
 <ul>
     {#each deps as path}
     <!-- file opening was dependent on GoldenLayout listening for known events on component registration -->
     <!-- with the new bridge, I can dynamically add listeners through componentState -->
-    <li on:click={e => dispatch('open', { path, newTab: e.ctrlKey })}>{ path }</li>
+    <li on:click={e => $__DIS__['ioc-editing-tools/openDependency'](gl_layout, path, e)}>{ path }</li>
     {/each}
 </ul>
